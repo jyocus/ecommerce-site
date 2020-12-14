c0a08f2f00e3311c08e3e16feee734e79d94605a
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-14 13:14:59 -0600

    Adding landing page component, switching over to branches

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.landing.css';
+
+function Landing() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Landing
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Header/Navbar'
+import React from 'react';
+import Navbar from '../components/Header/Navbar';
+import Landing from '../components/Landing/Landing';
 
 function Main() {
     return (
         <div>
             <Navbar />
+            <Landing />
         </div>
     )
 }
